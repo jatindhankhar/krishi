36058fc092664c74559d95a433bad74106a66edb
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-09-11 19:08:10 +0530

    Committed remaining files. Experimenting with a new library

--- app/src/main/java/in/jatindhankhar/krishi/MainActivity.java
+++ app/src/main/java/in/jatindhankhar/krishi/MainActivity.java
@@ -124,6 +124,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
     }
 
     private View.OnClickListener handleClick = new View.OnClickListener() {
